6170199374e808b58f0d009f83c8daa272d852ce
Luke McGregor <luke.mcgregor@gmail.com>
2012-08-14 20:21:05 +1200

    Basic stuff

---
+++ StaticVoid.Xml/DataContractSerialiser/DataContractSerialiserExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVoid.Xml.DataContractSerialiser
+{
+    public static class DataContractSerialiserExtensions
+    {
+        public static string Serialise<T>(this T entity) where T : class
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(memoryStream, entity);
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+        }
+
+        public static T Deserialise<T>(this string xml) where T : class
+        {
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                return (T)serializer.ReadObject(memoryStream);
+            }
+        }
+    }
+}
---
+++ StaticVoid.Xml/XmlSerialiser/XmlSerialiserExtentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace StaticVoid.Xml.XmlSerialiser
+{
+    public static class XmlSerialiserExtentions
+    {
+        public static string Serialise<T>(this T entity) where T : class
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(memoryStream, entity);
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+        }
+
+        public static T Deserialise<T>(this string xml) where T : class
+        {
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(memoryStream);
+            }
+        }
+    }
+}
